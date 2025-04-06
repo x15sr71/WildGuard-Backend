e9b8f1c0ad5d453016abbe5ed3fc28f62a17bdb5
Chandragupt Singh <chandraguptsingh1729@gmail.com>
2025-04-06 15:41:40 +0530

    fixed CORS

--- src/server.ts
+++ src/server.ts
@@ -34,9 +34,13 @@ app.use('/uploads', express.static(path.join(process.cwd(), 'public/uploads')));
 app.use(express.json({ limit: '100mb' })); // Move to top and increase limit
 app.use(express.urlencoded({ limit: '100mb', extended: true }));
 app.use(cors({
-  origin: "http://localhost:5173", // allow only your frontend origin
+  origin: [
+    "https://wildguard-client-642935703539.asia-south1.run.app", 
+    "http://localhost:5173"
+  ],
   credentials: true,
 }));
+
 
 app.use("/api/posts", postsRouter);
 
